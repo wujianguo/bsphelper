467bd3bad60b2e548573cdc1c314cd45d9785632
wujianguo <github@wujianguo.org>
2016-02-21 15:28:39 +0800

    bsp

--- app.py
+++ app.py
@@ -13,8 +13,26 @@ import logging
 class SlackHandler(tornado.web.RequestHandler):
 
     def post(self):
-        print(self.request.body)
-        logging.error(self.request.body)
+        pgyer = self.get_query_argument("pgyer", None)
+        if pgyer:
+            title_url = "https://www.pgyer.com/" + pgyer
+            image_url = "https://o1wjx1evz.qnssl.com/app/qrcode/" + pgyer
+            payload = {
+                "icon_url": "https://s3-us-west-2.amazonaws.com/buddybuild-public/assets/slack-lolo-icon.png",
+                "parse": "none",
+                "username": "buddybuild",
+                "attachments": [
+                    {
+                        "fallback": "湾视 iOS 新版发布",
+                        "title": "湾视 iOS 新版发布，点击或扫描二维码安装",
+                        "title_link": title_url,
+                        "image_url": image_url
+                    }
+                ]
+            }
+            requests.post("https://hooks.slack.com/services/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", data=json.dumps(payload))
+        else:
+            requests.post("https://hooks.slack.com/services/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", data=self.request.body)
         self.write("ok")
 
 settings = {
